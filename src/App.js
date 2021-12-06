47ca9a93bb73fa2c7f79b8f20fb8733c0680d53c
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-06 20:25:20 +0200

    Create Basic Login component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header/Header.js';
+import { Login } from './components/Login/Login.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Login />
     </div>
   );
 }
