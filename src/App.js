aefa76d5afd0a28395962c494648f5a3d2f13cae
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-06 20:28:24 +0200

    Create Basic Register component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/Header/Header.js';
 import { Login } from './components/Login/Login.js';
+import { Register } from './components/Register/Register.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Login />
+      <Register />
     </div>
   );
 }
