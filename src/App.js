924628c6a87b51a950013efd9779fa35d7535d01
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-06 20:33:38 +0200

    Create Basic Home component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Header } from './components/Header/Header.js';
 import { Login } from './components/Login/Login.js';
 import { Register } from './components/Register/Register.js';
+import { Home } from './components/Home/Home.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Login />
       <Register />
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import styles from './Home.module.css';
+export const Home = () => {
+  return (
+    <div className={styles['wrap-home']}>
+      <h1>Welcome to Beauty Zone</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        facilis nemo veniam nisi est possimus ex odio. Molestiae soluta illum
+        dolore rem unde saepe provident iure ad? Qui, dolor velit.
+      </p>
+    </div>
+  );
+};
