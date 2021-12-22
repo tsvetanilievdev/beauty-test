749f0e88c33cbcbbdb164d613a5a93e72b106f3d
Tsvetan Iliev <tsvetanilievdev@gmail.com>
2021-12-22 11:57:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+
+import requester from './services/requester.js';
+
 import { Header } from './components/Header/Header.js';
 import { Login } from './components/Login/Login.js';
 import { Register } from './components/Register/Register.js';
@@ -6,14 +9,12 @@ import { Home } from './components/Home/Home.js';
 import { Route, Routes } from 'react-router';
 import { Procedures } from './components/Procedures/Procedures.js';
 import { ProcedureDetails } from './components/Procedures/ProcedureDetails.js';
-import requester from './services/requester.js';
-
 
 window.api = requester;
 
 function App() {
   return (
-    <div className="App bg">
+    <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
